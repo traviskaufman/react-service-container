24ba8dfedae39573802e21110181321c6e5a614c
Travis Kaufman <travis.kaufman@gmail.com>
2020-06-24 10:45:55 -0400

    First parts of tests filled out

--- react-service-container.test.js
+++ react-service-container.test.js
@@ -1,37 +1,114 @@
 import { ServiceContainer, useService } from "./lib";
-import React, { useEffect } from "react";
+import React from "react";
 import renderer from "react-test-renderer";
 
-test("useValue", () => {
-  class Dep {
-    fn() {}
-  }
-  const mock = jest.fn();
-  const Component = () => {
-    const dep = useService(Dep);
-    useEffect(() => {
-      dep.fn();
-    }, []);
-  };
-  renderer.create(
-    <ServiceContainer
-      providers={[{ provide: Greeter, useValue: { fn: mock } }]}
-    >
-      <Component />
-    </ServiceContainer>
-  );
+class Dep {
+  fn() {}
+}
 
+const Component = () => {
+  const dep = useService(Dep);
+  return <p>{dep.fn()}</p>;
+};
+
+let mock;
+beforeEach(() => {
+  mock = jest.fn();
+});
+
+test("useValue", () => {
+  renderWithProviders([{ provide: Dep, useValue: { fn: mock } }]);
   expect(mock).toHaveBeenCalled();
 });
 
-test.todo("useClass");
+test("useClass", () => {
+  class MockClass {
+    fn() {
+      mock();
+    }
+  }
 
-test.todo("useFactory");
+  renderWithProviders([{ provide: Dep, useClass: MockClass }]);
+  expect(mock).toHaveBeenCalled();
+});
 
-test.todo("useExisting");
+test("useFactory", () => {
+  const factory = () => ({
+    fn: mock,
+  });
+
+  renderWithProviders([{ provide: Dep, useFactory: factory }]);
+  expect(mock).toHaveBeenCalled();
+});
+
+test("useExisting", () => {
+  class Existing {
+    fn() {
+      mock();
+    }
+  }
+
+  renderWithProviders([
+    { provide: Existing, useClass: Existing },
+    { provide: Dep, useExisting: Existing },
+  ]);
+  expect(mock).toHaveBeenCalled();
+});
+
+test("Class shorthand", () => {
+  class MockDep {
+    fn() {
+      mock();
+    }
+  }
+
+  const Component = () => {
+    const mockDep = useService(MockDep);
+    return <p>{mockDep.fn()}</p>;
+  };
+
+  renderer.create(
+    <ServiceContainer providers={[MockDep]}>
+      <Component />
+    </ServiceContainer>
+  );
+  expect(mock).toHaveBeenCalled();
+});
+
+test("Injection Tokens", () => {
+  const CONFIG = Symbol.for("config");
+  const config = { message: "Hello, world!" };
+
+  const Component = () => {
+    const { message } = useService(CONFIG);
+    return <p>{message}</p>;
+  };
+  const component = renderer.create(
+    <ServiceContainer providers={[{ provide: CONFIG, useValue: config }]}>
+      <Component />
+    </ServiceContainer>
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test.todo("useExisting with injection tokens");
 
 test.todo("Hierarchal injection");
 
-test.todo("Injection Tokens");
-
 test.todo("Class components");
+
+test.todo("Descriptive error when missing provider");
+
+test.todo("Descriptive error when provider function throws");
+
+test.todo("Descriptive error message when useExisting lookup fails");
+
+test.todo("Warn when overriding existing provider (in development?)");
+
+function renderWithProviders(providers) {
+  return renderer.create(
+    <ServiceContainer providers={providers}>
+      <Component />
+    </ServiceContainer>
+  );
+}
